8e149fc5d27e9a43bd78e6b7bbf1daa7dcab636d
Sakib669 <stewieboyyy@gmail.com>
2022-11-09 21:00:39 +0600

    abar korlam react a ponditi

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './Card.css';
 import Plus from '../logo.svg'
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlus } from '@fortawesome/free-solid-svg-icons';
 
 
 const Card = (props) => {
-    console.log(props)
+    // console.log(props)
 
     let {Course, Price , Image2} = props.count;
     
@@ -18,7 +19,7 @@ const Card = (props) => {
             <h2 className='product-name'>Product Name: {Course}</h2>
             <br />
             <h5>Price: {Price}$</h5>
-            <button onClick={() => props.handleClick(props.count)} className='add-cart'>Add to Cart</button>
+            <button onClick={() => props.handleClick(props.count)} className='add-cart'> <FontAwesomeIcon icon={faPlus}></FontAwesomeIcon> Add to Cart</button>
            </div>
         </div>
     );
